df0c2484cb01cb2744bf0b49c83af0bd48dfa2be
jvnwon <junwon@vaultmicro.com>
2022-08-05 16:29:15 +0900

    enhancement: Fix TodoList

--- src/components/molecules/TodoList/TodoList.tsx
+++ src/components/molecules/TodoList/TodoList.tsx
@@ -4,15 +4,16 @@ import { Todo } from '@usertypes/todos';
 
 import { Wrapper, Item } from './TodoList.style';
 
-const TodoList = ({ todos }: { todos: Todo[] }) => {
+const TodoList = ({ todos }: { todos?: Todo[] }) => {
   const navigate = useNavigate();
-  console.log(todos);
+
   return (
     <Wrapper>
       <Item sx={{ textAlign: 'center' }} onClick={() => navigate('./')}>
         +
       </Item>
-      {todos.length !== 0 &&
+      {todos &&
+        todos.length !== 0 &&
         todos.map((todo) => (
           <Item onClick={() => navigate(`./${todo.id}`)}>{todo.title}</Item>
         ))}
