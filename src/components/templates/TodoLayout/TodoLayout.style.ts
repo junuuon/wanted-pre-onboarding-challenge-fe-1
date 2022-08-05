10dcee6df049d0c9524f565cb093acaf39ff9982
jvnwon <junwon@vaultmicro.com>
2022-08-05 16:51:20 +0900

    enhancement: Change TodoLayout style

--- src/components/templates/TodoLayout/TodoLayout.style.ts
+++ src/components/templates/TodoLayout/TodoLayout.style.ts
@@ -1,8 +1,27 @@
 import styled from 'styled-components';
+import { Paper } from '@mui/material';
+import { media } from '@styles/theme';
 
 export const AppContent = styled.div`
-  flex: 1;
+  height: calc(100% - 64px);
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  height: 100%;
+  padding: 5rem;
+`;
+
+export const Wrapper = styled(Paper)`
   display: flex;
-  flex-direction: column;
-  max-width: 100%;
+  height: 100%;
+  width: 100%;
+
+  ${media.mobile} {
+    flex-direction: column-reverse;
+  }
+`;
+
+export const TodoWrapper = styled.div`
+  width: 100%;
 `;
--- src/components/templates/TodoLayout/TodoLayout.tsx
+++ src/components/templates/TodoLayout/TodoLayout.tsx
@@ -1,19 +1,49 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { getTodos } from '@api/todos';
+import TodoList from '@components/molecules/TodoList';
 import Navbar from '@components/organisms/Navbar';
 import AuthGuard from '@components/templates/AuthGuard';
+import useAuth from '@hooks/useAuth';
+import { Todo as TodoType } from '@usertypes/todos';
 
-import { AppContent } from './TodoLayout.style';
+import { AppContent, Content, Wrapper, TodoWrapper } from './TodoLayout.style';
 
-const TodoLayout: React.FC<{ children?: ReactNode }> = ({ children }) => (
-  <AuthGuard>
-    <AppContent>
-      <Navbar />
-      {children}
-      <Outlet />
-    </AppContent>
-  </AuthGuard>
-);
+const TodoLayout: React.FC<{ children?: ReactNode }> = ({ children }) => {
+  const [todos, setTodos] = useState<TodoType[]>();
+  const { token } = useAuth();
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await getTodos(token);
+        setTodos(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTodos();
+  }, [token]);
+
+  return (
+    <AuthGuard>
+      <AppContent>
+        <Navbar />
+        <Content>
+          {todos && (
+            <Wrapper>
+              <TodoList todos={todos} />
+              <TodoWrapper>
+                {children}
+                <Outlet />
+              </TodoWrapper>
+            </Wrapper>
+          )}
+        </Content>
+      </AppContent>
+    </AuthGuard>
+  );
+};
 
 export default TodoLayout;
